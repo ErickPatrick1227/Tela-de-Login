5ad1fe4f9e218670edcf2ae8c3a24f7697911439
Erick Patrick <erickckpatrick17@gmail.com>
2025-03-27 15:49:39 -0300

    arquivo com a conexão ao banco de dados

---
+++ meu-app/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+const port = 3000;
+
+//Conexção ao banco de dados
+mongoose
+  .connect("mongodb://localhost:27017/login")
+  .then(() => {
+    console.log("Conectado ao MongoDB");
+  })
+  .catch((err) => {
+    console.error("Erro ao conectar ao MongoDB", err);
+  });
+
+// Iniciar servidor
+app.listen(3000, () => console.log("Servidor rodando na porta 5000"));
