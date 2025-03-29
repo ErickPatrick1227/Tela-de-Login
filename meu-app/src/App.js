c62483bcd7c846c1740892b11915d97365fa1c79
Erick Patrick <erickckpatrick17@gmail.com>
2025-03-29 17:40:53 -0300

    melhoria nas alternações entre as paginas de login e cadastro

--- meu-app/src/App.js
+++ meu-app/src/App.js
@@ -1,22 +1,51 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  useNavigate,
+  useLocation,
+} from "react-router-dom";
 import React, { useState } from "react";
 import Login from "./Login";
 import Cadastro from "./Cadastro";
+import Dashboard from "./Dashboard"; // Importando a nova página
 
 function App() {
-  const [pagina, setPagina] = useState("login");
-
   return (
-    <div className="App">
-      {pagina === "login" ? <Login /> : <Cadastro />}
-      <button
-        onClick={() => setPagina(pagina === "login" ? "cadastro" : "login")}
-      >
-        {pagina === "login"
-          ? "Não tem uma conta? Cadastre-se"
-          : "Já tem conta? Faça login"}
-      </button>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/cadastro" element={<Cadastro />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+        <NavigationButtons />
+      </div>
+    </Router>
   );
 }
+
+//botão para alternar entre login e cadastro
+const NavigationButtons = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  // Só exibe o botão se estiver na página de Login ou Cadastro
+  if (location.pathname !== "/" && location.pathname !== "/cadastro") {
+    return null;
+  }
+  return (
+    <button
+      onClick={() =>
+        navigate(location.pathname === "/cadastro" ? "/" : "/cadastro")
+      }
+    >
+      {window.location.pathname === "/cadastro"
+        ? "Já tem conta? Faça login"
+        : "Não tem uma conta? Cadastre-se"}
+    </button>
+  );
+};
 
 export default App;
